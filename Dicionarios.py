47976c1f0da20319d960f6162b29fd6c77dc9f96
Murilo <murilomos@hotmail.com>
2026-08-02 20:04:18 -0300

    Domingou

---
+++ Dicionarios.py
@@ -0,0 +1,8 @@
+dictionary = {"gato": "chat", "cachorro": "chien", "cavalo": "cheval"}
+phone_numbers = {'chefe' : 5551234567, 'Suzy' : 22657854310}
+empty_dictionary = {}
+
+# Imprima os valores aqui.
+print(dictionary)
+print(phone_numbers)
+print(empty_dictionary)
